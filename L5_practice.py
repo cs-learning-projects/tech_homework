6f3b8d6a4159d6ebf56a63ec4cd29308327054c9
nepogodda <nepogoda@Olgas-MacBook-Pro.local>
2017-02-08 23:17:58 +0300

    updates

--- L5_practice.py
+++ L5_practice.py
@@ -1,27 +1,38 @@
-class Products(object):
-    def __init__(self, name, quantity, price):
-    	self.name = name
-    	self.quantity = quantity
-    	self.price = price
+class Abstract_product(object):
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
 
+class Purchase_price_total(object):
+    #def __init__(self):
+    #    self.products = []
 
-    def product_price_calc(self, price, quantity):
-    	total_price = self.price * self.quantity
-    	return total_price
-
-class Product_by_weight(Products):
-	def __init__(self, name, weight, price):
-	    super().__init__(name, price)
-	    self.weight = weight
+    def product_price_calc(self, name, price, quantity):
+        total_price = price * quantity
+        print(total_price, quantity)
+        return total_price
     
 
-class Product_by_unit(Products):
+class Product_by_weight(Abstract_product):
+    def __init__(self, name, price, weight):
+        super().__init__(name, price)
+        self.weight = weight
+    
+
+class Product_by_unit(Abstract_product):
+    #def __init__(self, name, price, unit):
+    #    super().__init__(name, price)
+    #    self.unit = unit
     pass
 
-class Product_by_volume(Products):
+class Product_by_volume(Abstract_product):
+    #def __init__(self, name, price, volume):
+    #    super().__init__(name, price)
+    #    self.volume = volume
     pass
 
 w1 = Product_by_weight('meat', 2, 200)
-print(w1.name, w1.quantity, w1.price)
+print(w1.name, w1.weight, w1.price)
+c = Purchase_price_total()
+c.product_price_calc(w1.name, w1.price, w1.weight)
 
-print(w1.product_price_calc(w1.price, w1.quantity))
