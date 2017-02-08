145d3b63afac03b8ce370f037398ef0c9cd15b19
nepogodda <nepogoda@Olgas-MacBook-Pro.local>
2017-02-09 02:09:41 +0300

    further updates

--- L5_practice.py
+++ L5_practice.py
@@ -1,38 +1,59 @@
+import math
+
 class Abstract_product(object):
+    volume_descripton = None
     def __init__(self, name, price):
         self.name = name
         self.price = price
 
 class Purchase_price_total(object):
-    #def __init__(self):
-    #    self.products = []
-
-    def product_price_calc(self, name, price, quantity):
-        total_price = price * quantity
-        print(total_price, quantity)
+    def product_price_calc(self, name, price, quantity, q_description):
+        if q_description == 'unit(s)' and quantity >= 2:
+            total_price = (price * quantity) * 0.95
+            print(total_price, 'rubles (with 5% discount) for', quantity, q_description, 'of', name)
+        elif q_description == 'kg':
+            rounding = math.modf(quantity)
+            if rounding[0] > 0.5:
+                quantity = rounding[1] + 1
+            else:
+                quantity = rounding[1]
+            total_price = price * quantity
+            print(total_price, 'rubles for', quantity, q_description, '(rounded to 50g) of', name)
+        else:
+            total_price = price * quantity
+            print(total_price, 'rubles for', quantity, q_description, 'of', name) #как убрать пробел между суммой и rubles
         return total_price
     
 
 class Product_by_weight(Abstract_product):
+    volume_descripton = 'kg'
     def __init__(self, name, price, weight):
         super().__init__(name, price)
         self.weight = weight
     
 
 class Product_by_unit(Abstract_product):
-    #def __init__(self, name, price, unit):
-    #    super().__init__(name, price)
-    #    self.unit = unit
-    pass
+    volume_descripton = 'unit(s)'
+    def __init__(self, name, price, unit):
+        super().__init__(name, price)
+        self.unit = unit
+
 
 class Product_by_volume(Abstract_product):
-    #def __init__(self, name, price, volume):
-    #    super().__init__(name, price)
-    #    self.volume = volume
-    pass
+    volume_descripton = 'litre'
+    def __init__(self, name, price, volume):
+        super().__init__(name, price)
+        self.volume = volume
+    
+# данные для проврки работы программы
+w1 = Product_by_weight('meat', 4, 2.2)
+purchase_w1 = Purchase_price_total()
+purchase_w1.product_price_calc(w1.name, w1.price, w1.weight, w1.volume_descripton)
 
-w1 = Product_by_weight('meat', 200, 2)
-print(w1.name, w1.weight, w1.price)
-c = Purchase_price_total()
-c.product_price_calc(w1.name, w1.price, w1.weight)
+u1 = Product_by_unit('chips', 2, 2)
+purchase_u1 = Purchase_price_total()
+purchase_u1.product_price_calc(u1.name, u1.price, u1.unit, u1.volume_descripton)
 
+v1 = Product_by_volume('pepsi', 1, 3)
+purchase_u1 = Purchase_price_total()
+purchase_u1.product_price_calc(v1.name, v1.price, v1.volume, v1.volume_descripton)
