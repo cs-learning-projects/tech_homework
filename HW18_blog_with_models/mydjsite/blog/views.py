52d1bb19a7ea8e74e48cb5f56a4583366d2b6dc9
nepogodda <nepogoda@Olgas-MacBook-Pro.local>
2017-04-08 22:39:07 +0300

    HW18 django blog with models

---
+++ HW18_blog_with_models/mydjsite/blog/views.py
@@ -0,0 +1,74 @@
+from django.db.models import Count, Avg
+from django.db.models.functions import TruncMonth
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Comment
+from django.utils import timezone
+from .forms import PostForm, CommentForm
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts' : posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
+@login_required
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post,pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+
+def posts_statistics(request):
+    posts = Post.objects.count()
+    comments = Comment.objects.count()
+    avg_per_post = comments / posts
+    avg_per_month = Post.objects.annotate(
+        month=TruncMonth('published_date')).values('month').annotate(count=Count('title'))
+    return render(request, 'blog/statistics.html', {'posts': posts, 'comments': comments,
+                                                    'avg_per_post': avg_per_post, 'avg_per_month': avg_per_month})
